9e0109a00ce57f067ab5ad5f421c91e5b9c05951
PUQ <it@puq.pl>
2021-04-28 18:01:49 +0200

    Delete english.php

--- lang/english.php
+++
@@ -1,7 +0,0 @@
-<?php
-$_LANG_PUQ['server_address'] = "Server address";
-$_LANG_PUQ['username'] = "Username";
-$_LANG_PUQ['password'] = "Password";
-$_LANG_PUQ['info_1'] = "You can log in using your Username/Password";
-
-
