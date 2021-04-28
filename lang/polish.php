0068a0c0482b276b466abfbf9eafcfd8f3423587
PUQ <it@puq.pl>
2021-04-28 18:01:55 +0200

    Delete polish.php

--- lang/polish.php
+++
@@ -1,6 +0,0 @@
-<?php
-$_LANG_PUQ ['server_address'] = "Adres serwera";
-$_LANG_PUQ ['username'] = "Nazwa użytkownika";
-$_LANG_PUQ ['password'] = "Hasło";
-$_LANG_PUQ ['info_1'] = "Możesz zalogować się przy użyciu swojej nazwy użytkownika i hasła";
-
